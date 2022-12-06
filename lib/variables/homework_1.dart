b3eafb4bcd1e0c7d50a4de60433c1e587ef4cbb8
Hope Coding <ucan.aldirmaz@gmail.com>
2022-12-07 01:16:33 +0300

    First Commit.
    Özel değişken oluşturma ödev(Flutter Bootcamp)

---
+++ lib/variables/homework_1.dart
@@ -0,0 +1,51 @@
+import 'dart:ffi';
+
+void main() {
+  String city = "İstanbul";
+  String country = "Türkiye";
+  int phone = 05073063030;
+  int postcode = 30100;
+  String email = "arif@gmail.com";
+  String job = "Mühendis";
+  final int amount_of_stock = 100;
+  String customer_name = "Zelda";
+  final int balance = 1500;
+  String birthday = "01.08.1997";
+  final int salary = 15000;
+  String marial_status = "Evli";
+  String product_comment = "Çok güzel";
+  String payment_date = "21.10.22";
+  bool payment = true;
+  int ordered = 1080;
+  String car_model = "Audi A8";
+  String book_name = "Evliya";
+  String publish_date = "31.12.2022";
+  double rate_of_discount = 0.50;
+  int number_of_rooms = 10;
+  String latitude = "15°N";
+  String longitude = "30°E";
+  String product_name = "Falım";
+  int food_price = 200;
+  String brand = "Logitech";
+  String music_name = "Suavemente";
+  double video_time = 27.20;
+  int product_point = 5;
+  String picture_name = "Siyah deniz";
+  String file_format = ".exe";
+  String color = "Blue";
+  String color_code = 'B';
+  String phone_model = "Huawei";
+  double phone_size = 5.8;
+  int weight = 70;
+  String national_day = "10 Kasım";
+  String holiday_day = "Cumartesi";
+  String reservation_date = "12.10.2100";
+  String street_name = "Gül";
+  String minibus_route = "Hat 2(Küçük Sanayi)";
+  int minutes_remaining = 15;
+  int tracking_code = 001;
+  int coupon_duration = 100;
+  int coupon_code = 0120130410;
+  String billing_address = "4182 North Valley Lake Drive, Fayetteville AR 72703";
+
+}
